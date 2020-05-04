92d73f960cbad052e4297c48bb880b6c1172d4e8
Will Laws <willhlaws@gmail.com>
2020-05-04 21:36:14 +1000

    refactor to be a basic pipeline

---
+++ jenkins/scripts/deploy-for-alpha.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+echo "deploy steps for alpha go here"
+sleep 5
+echo "alpha has deployed"
---
+++ jenkins/scripts/deploy-for-beta.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+echo "deploy steps for beta go here"
+sleep 5
+echo "beta has deployed"
